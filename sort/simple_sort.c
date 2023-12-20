01c2cc012ca4be4fae6c6aabb0cc7deed3d67f08
pgrossma <pgrossma@student.42heilbronn.de>
2023-12-20 12:55:39 +0100

    working 3, 4 and 5 sorting functions

--- sort/simple_sort.c
+++ sort/simple_sort.c
@@ -6,7 +6,7 @@
 /*   By: pgrossma <pgrossma@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 20:11:45 by pgrossma          #+#    #+#             */
-/*   Updated: 2023/12/20 12:26:12 by pgrossma         ###   ########.fr       */
+/*   Updated: 2023/12/20 12:49:54 by pgrossma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ void	ft_sort_three(t_list **stack)
 	a = (*stack)->nbr;
 	b = (*stack)->next->nbr;
 	c = (*stack)->next->next->nbr;
-
 	if (a > b && b < c && c > a)
 		sa(stack, 1);
 	if (a > b && b > c && c < a)
@@ -48,24 +47,30 @@ void	ft_sort_three(t_list **stack)
 
 void	ft_sort_four(t_list **stack_a, t_list **stack_b)
 {
-	int	current_index;
+	int	i;
+	int	lst_size;
 
 	ft_pre_index(stack_a);
-	current_index = (*stack_a)->index;
-	while (current_index != 0)
+	i = 0;
+	lst_size = ft_lstsize(*stack_a);
+	while (i < lst_size - 3)
 	{
-		if (current_index == 1)
+		if ((*stack_a)->index == 0)
+		{
 			pb(stack_a, stack_b, 1);
+			i++;
+		}
 		else
 			ra(stack_a, 1);
-		current_index = (*stack_a)->index;
 	}
+	ft_sort_three(stack_a);
+	pa(stack_a, stack_b, 1);
 }
 
 void	ft_sort_five(t_list **stack_a, t_list **stack_b)
 {
-	int		i;
-	int		lst_size;
+	int	i;
+	int	lst_size;
 
 	ft_pre_index(stack_a);
 	i = 0;
@@ -80,11 +85,9 @@ void	ft_sort_five(t_list **stack_a, t_list **stack_b)
 		else
 			ra(stack_a, 1);
 	}
-	// ft_print_stack(*stack_a);
 	ft_sort_three(stack_a);
 	if ((*stack_b)->nbr < (*stack_b)->next->nbr)
 		sb(stack_b, 1);
 	pa(stack_a, stack_b, 1);
 	pa(stack_a, stack_b, 1);
-	// ft_print_stack(*stack_a);
 }
